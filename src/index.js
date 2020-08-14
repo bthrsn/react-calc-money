c67d67b3f247e33ace5053e55d899ed1c56443fc
Mikhail <mikhail.lhts@gmail.com>
2020-08-14 22:46:44 +0300

    Add html to App.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./normalize.css";
+import "./style.css";
+
+// компоненты пишуться с большой буквы
+// создадим кнопку с помощью реакта, свойств(pops) не будет
+// const Test = () => React.createElement("button", null, "Привет!");
+
+// кнопка на чистом jsx, обычно создается вот так, потому что весит меньше
+// const Test2 = () => <button>Привет!</button>;
+
+// Создадим функциональный компонент
+// По id в нативном js мы его найти не сможем
+// const Header = ({ text }) => <h1 id="h1">{text}</h1>;
+
+// создаем Элемент
+// const Element = <p>Параграф</p>;
+
+// создаем классовый компонент
+// class Main extends React.Component {
+//   render() {
+//     return (
+//       <div className="main">
+//         <Header text={"свойство текст" + this.props.count} />
+//         {Element}
+//       </div>
+//     );
+//   }
+// }
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
