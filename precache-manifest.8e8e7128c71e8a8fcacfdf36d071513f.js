a1f14399f172c45e906487605cc0ffaa49212b36
Mikhail <mikhail.lhts@gmail.com>
2020-08-18 22:48:21 +0300

    Updates

---
+++ precache-manifest.8e8e7128c71e8a8fcacfdf36d071513f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5d19ee9febcf19ceba93f760a5db116",
+    "url": "/react-calc-money/index.html"
+  },
+  {
+    "revision": "d204d89a92e12b177136",
+    "url": "/react-calc-money/static/css/main.d41691d1.chunk.css"
+  },
+  {
+    "revision": "ab12b72ec6cd5010e57a",
+    "url": "/react-calc-money/static/js/2.42df7b18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calc-money/static/js/2.42df7b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d204d89a92e12b177136",
+    "url": "/react-calc-money/static/js/main.2e2da5c1.chunk.js"
+  },
+  {
+    "revision": "2ea2fe6b0eb4f0ee8282",
+    "url": "/react-calc-money/static/js/runtime-main.1c82780e.js"
+  }
+]);
